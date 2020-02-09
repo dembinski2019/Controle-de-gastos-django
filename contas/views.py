4c93838d075135f1edc2e1472590521804af188c
dembinski2019 <everton_s.d@hotmail.com>
2020-02-09 14:37:20 -0300

    ate tamplates

--- contas/views.py
+++ contas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def home(request):
+    data={}
+    data['now'] = datetime.datetime.now()
+    #html= f"<html><body> agora é {now}.</body><html>"
+    return render(request, 'contas/home.html',data)
